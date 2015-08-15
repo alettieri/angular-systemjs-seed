c93bb3a0b69a3ee609e2550edda45e75ee01f5fd
Antonio Lettieri <alettieri@gmail.com>
2015-08-14 18:38:34 -0700

    Go Go es6 classes

--- src/app/views/main/main.controller.js
+++ src/app/views/main/main.controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
-function MainController() {
-    var vm = this;
-    vm.title = 'Main Body';
-    vm.currentDate = (new Date()).toDateString();
+class MainController {
+    constructor() {
+      var vm = this;
+      vm.title = 'Main Body';
+      vm.currentDate = (new Date()).toDateString();
+    }
 }
 
 export default MainController;
